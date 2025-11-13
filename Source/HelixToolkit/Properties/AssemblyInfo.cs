7a0fa38cfdea055529f48f4c7c4e756cb09d360b
Henry LaFleur <lafleurh@gmail.com>
2025-11-13 16:48:04 -0600

    Move assembly metadata to project file
    AssemblyTitle and Description attributes were removed from AssemblyInfo.cs and added to HelixToolkit.csproj, centralizing assembly metadata in the project file for better maintainability.

--- Source/HelixToolkit/Properties/AssemblyInfo.cs
+++ Source/HelixToolkit/Properties/AssemblyInfo.cs
@@ -5,6 +5,3 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System.Reflection;
-
-[assembly: AssemblyTitle("Helix 3D Toolkit core library")]
-[assembly: AssemblyDescription("Core library containing classes shared by different platforms.")]
